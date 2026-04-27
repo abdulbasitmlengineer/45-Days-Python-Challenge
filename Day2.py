8918e323ef73891754dae9a9b08f18b89bcea1d6
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-04-27 15:45:00 +0500

    Day 2: Variables and Data Types in Python.py
    Today I learned how to store information in computer memory using Variables. I practiced different Data Types including Strings (text), Integers (numbers), and Floats (decimal numbers). [span_2](start_span)I also learned how to use f-strings to print variables inside sentences.

---
+++ Day2.py
@@ -0,0 +1,6 @@
+Name = "Abdul Basit"
+Ambition = "Ml Engineer"
+Class = 8
+print(f"My name is {Name}")
+print(f"I want to become an {Ambition} in the future")
+print(f"In 2026 I Study In {Class} class")
