c81db187930f1efbf34a422d81f7c45c2f2c2a11
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-04-28 19:19:24 +0500

    Day 3: Basic Math Operations and Arithmetic in Python.py
    Today I used Python as a calculator to perform addition, subtraction, multiplication, and division. I also combined these operations with f-strings to display the output clearly.

---
+++ Day3.py
@@ -0,0 +1,12 @@
+num1 = 100
+num2 = 51
+sum_result = num1 + num2
+sub_result = num1 - num2
+mul_result = num1 * num2
+div_result = num1 / num2
+print(f"First Number : {num1}")
+print(f"Second Number: {num2}")
+print(f"Addition {num1} + {num2} = {sum_result}")
+print(f"Subtraction: {num1} + {num2} = {sub_result}")
+print(f"Multiplication: {num1} * {num2} = {mul_result}")
+print(f"Division: {num1} / {num2} = {div_result}")
