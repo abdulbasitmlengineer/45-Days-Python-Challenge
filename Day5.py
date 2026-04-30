9b32e0b6dc2aa8a76d124857cdd2174cf5aff2a5
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-04-30 14:16:40 +0500

    Day 5: Mastered String Formatting and Concatenation.py
    The more I learn Python, the more I realize that writing clean code is an art. Today, on Day 5 of my 45-Day Challenge, I focused on String Formatting.

---
+++ Day5.py
@@ -0,0 +1,10 @@
+Name = "AbdulBasit"
+Goal = "MlEngineer"
+Class = 8
+Country = "Pakistan"
+City = "Rawalpindi" 
+Status = "Student"
+School = "FG PUBLIC HIGH SCHOOL"
+print(f"Mera Naam {Name} hai .")
+print(f"Main {Country} ke Sheher {City} Main {School} Mein {Class} class Mein aik {Status} hun ")
+print(f"Aur Main Future Main {Goal} Banna Chahta hu...")
