509718c9b0a2d6f1847b948c9f3ba47ea38b0d16
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-05-01 10:30:57 +0500

    day6_age_calc.py
    Built my first functional tool: An Age Calculator that takes birth year and current year to provide dynamic results.

---
+++ Day6.py
@@ -0,0 +1,18 @@
+print("A-O-A ...Want to Know Your Age??? .....")
+
+Name = input("What Is Your Name?     ")
+Age = input("In Which Year  you born???       ")
+
+CurrentYear = input("Write Current Year:      ")
+
+integer2 = int(CurrentYear)
+
+integer1 = int(Age)
+
+print(f"Hello {Name} You Are {integer2 - integer1} Old")
+
+print("Nice to meet you .......")
+
+print("Allah Hafiz")
+
+
