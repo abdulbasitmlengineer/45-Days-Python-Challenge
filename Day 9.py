3a4b44ee935c8774e8bd47c2a2f06cfb9cf575da
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-05-04 14:25:14 +0500

    Day 9....Operations.py
    Today I Learn operation Like == <>

---
+++ Day 9.py
@@ -0,0 +1,14 @@
+Name = "Abdul Basit"
+Number = "400"
+guess = "20"
+Number = int(Number)
+guess = int(guess)
+print("_________________________")
+if (guess * guess) == Number:
+    print("True")
+if (guess * guess) != Number:
+    print("False")
+if (guess * guess) > Number:
+    print("Aap Ka Number Barha hai.")
+if (guess * guess) < Number:
+    print("Aap ka nunber choota hai..")
