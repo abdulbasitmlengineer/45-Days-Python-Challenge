5289251f735b7a3e0394230e81d4aceb8c3ed8a7
abdulbasitmlengineer <abdulbasitmlengineer@gmail.com>
2026-05-12 22:28:31 +0500

    Understaning of Ruples.py
    Today I Understand The ruples ....

---
+++ Day17.py
@@ -0,0 +1,8 @@
+bank_codes = ("PK01", "PK02", "PK03")
+
+print("Bank Codes:", bank_codes)
+
+print("Main Branch:", bank_codes[0])
+print("Secondary Branch:", bank_codes[1])
+
+bank_codes[0] = "UK01"  
